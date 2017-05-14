6002e99e5fc5bb1013042f05503e22dc25ad80dc
palme <palme@DESKTOP-VLBDHD1>
2017-05-13 19:46:50 -0700

    5/13/17 - Finally done

---
+++ src/byui/cit1260/theHunted/model/Animal.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit1260.theHunted.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author palme
+ */
+public class Animal implements Serializable {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    // Instance variables
+    private String name;
+    private double bestTime;
+
+    public Animal() {
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBestTime() {
+        return bestTime;
+    }
+
+    public void setBestTime(double bestTime) {
+        this.bestTime = bestTime;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Animal other = (Animal) obj;
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "name=" + name + ", bestTime=" + bestTime + '}';
+    }
+    
+    
+    
+}
---
+++ src/byui/cit1260/theHunted/model/weapons.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit1260.theHunted.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author palme
+ */
+public class weapons implements Serializable{
+    
+    /**
+     * @param args the command line argument
+     */
+    public static void main(String[] args) {
+        
+        // TODO code
+    }
+    
+    // class instance variables
+    private String name;
+    private double bestTime;
+
+    public weapons() {
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBestTime() {
+        return bestTime;
+    }
+
+    public void setBestTime(double bestTime) {
+        this.bestTime = bestTime;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final weapons other = (weapons) obj;
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "weapons{" + "name=" + name + ", bestTime=" + bestTime + '}';
+    }
+    
+    
+    
+}
