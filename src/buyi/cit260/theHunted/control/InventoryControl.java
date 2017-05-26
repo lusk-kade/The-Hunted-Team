3ad678c5cdabfd73e2bf5d5fbfd2e7767e40bba6
benedictj <benedictj@LIVADM-IT143.fosterfarms.com>
2017-05-26 13:00:22 -0700

    Updated group work

--- src/buyi/cit260/theHunted/control/InventoryControl.java
+++ src/buyi/cit260/theHunted/control/InventoryControl.java
@@ -9,19 +9,51 @@ package buyi.cit260.theHunted.control;
  *
  * @author lus12
  */
-public class InventoryControl {
+public class InventoryControl { 
     
-  public double calcAnimalPoints(double name, double weight){
-  
+    private double nameValue;
+
+
+  public double calcAnimalPoints(String name, double weight){
+
        double pointScalar = .1;
-       
-	IF (name != name.value){
+
+
+
+	if (name == null) {
 		return -2;
         }
-	IF(weight < 3 OR weight > 12){
+
+        if (!"rabbit".equals(name)) {
+                return -2;
+        }
+
+	if (weight < 3) {
                 return -1;
         }
-        double points = (weight * name.value) / pointScalar;
+
+        if (weight > 12) {
+                return -1;
+        }
+
+        if ("rabbit".equals(name)) {
+                nameValue = 1;
+        }
+
+        if ("deer".equals(name)) {
+                nameValue = 80;
+        }
+
+        if ("moose".equals(name)) {
+                nameValue = 125;
+        }
+
+        if ("bear".equals(name)) {
+                nameValue = 200;
+        }
+
+        double points = (weight * nameValue) / pointScalar;
                 return points;
   }
 }
+
